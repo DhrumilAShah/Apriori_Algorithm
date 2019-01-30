df430d9a638a9e451c58478497d4240d24f4fbb6
Dhrumil Shah <36946750+DhrumilAShah@users.noreply.github.com>
2019-01-30 17:52:55 -0500

    Add files via upload

---
+++ apriori.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from pandas import DataFrame as df
+import numpy as np
+
+df = pd.read_excel('stop&shop.xlsx',header=None)
+data = np.array(df.as_matrix([1]))
+print(data)
+
+def getCandidateMap(arr):
+	candMap = {}
+	for x in np.nditer(arr, flags=['refs_ok'],op_flags=['readwrite']):
+		trans = np.array(str(x).split(','))
+		for i in np.nditer(trans):
+			i=str(i).strip()
+			if i in candMap:
+				candMap[i] = candMap[i]+1
+			else:
+				candMap[i]=1
+	return candMap
+
+
+def getSupport(data,*args):
+	counter=0	
+	trans = np.array(data)
+	args = np.array(args)
+	for x in np.nditer(trans, flags=['refs_ok'],op_flags=['readwrite']):
+		items = np.array(str(x).split(','))	
+		checkArr=np.array([])
+		for i in np.nditer(args):
+			if((i in items)==True):
+				checkArr = np.append(checkArr, True)
+			else:
+				checkArr = np.append(checkArr, False)	
+			if((False in checkArr)==False):
+				counter=counter+1
+			checkArr=np.array([])	
+	return counter
+
+
+#print(getCandidateMap(data))
+print()
+print(getSupport(data,"apple","banana"))	

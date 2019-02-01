7ecad3bfe79c4fcbec33586fd1ba02e46460121f
DhrumilaShah <dhrumilshah2495@gmail.com>
2019-01-31 19:54:40 -0500

    apriori

--- apriori.py
+++ apriori.py
@@ -2,36 +2,48 @@ import pandas as pd
 from pandas import DataFrame as df
 import numpy as np
 
-df = pd.read_excel('stop&shop.xlsx',header=None)
-data = np.array(df.as_matrix([1]))
+df = pd.read_excel('dataset/stop&shop.xlsx',header=None)
+data = np.array(df[1].values)#as_matrix
 #print(data)
 
-def getCandidateMap(arr):
+def transform(data):
 	candMap = {}
-	for x in np.nditer(arr, flags=['refs_ok'],op_flags=['readwrite']):
+	for x in np.nditer(data, flags=['refs_ok'],op_flags=['readwrite']):
 		trans = np.array(str(x).split(','))
 		for i in np.nditer(trans):
-			i=str(i).strip()
-			if i in candMap:
-				candMap[i] = candMap[i]+1
-			else:
-				candMap[i]=1
+			i = str(i).strip()
+			candMap[(i)] = candMap[i]+1 if i in candMap else 1
 	return candMap
 
-
-def getSupport(data,*args):
-	counter=0	
+def getItemCount(data,*args):
+	counter = 0
 	trans = np.array(data)
 	args = np.array(args)
 	for x in np.nditer(trans, flags=['refs_ok'],op_flags=['readwrite']):
-		items = np.array(str(x).strip().split(','))	
+		items = np.array(str(x).strip().split(','))
 		checkArr=np.array([])
-		for i in np.nditer(args):checkArr = np.append(checkArr, True if (i in items) else False)							
+		for i in np.nditer(args):checkArr = np.append(checkArr, True if (i in items) else False)
 		if((False in checkArr)==False):counter+=1
-		checkArr=np.array([])	
+		checkArr=np.array([])
 	return counter
 
+def prune(candidateMap,minSupport):
+	for key, value in candidateMap.items():
+		if(value < minSupport):del candidateMap[key]
+	return candidateMap
 
-#print(getCandidateMap(data))
-#print()
-print(getSupport(data,"apple","banana"))	
+def join(candidateKeys):
+	candidateKeys = np.array(candidateKeys)
+	arr = np.array([])
+	counter = 0
+	for y in np.nditer(candidateKeys[:-1]):
+		for x in np.nditer(candidateKeys[:-2]):
+			arr = np.append(arr,(candidateKeys[counter],candidateKeys[counter+1]))
+		counter+=1
+	print(arr)
+	return
+
+
+print(join(['as','bs','cs','ds']))
+#print(transform(data))
+#print(getItemCount(data,"apple","banana"))
